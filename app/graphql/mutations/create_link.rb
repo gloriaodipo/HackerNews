e063066379426af58f799135d33e857a05cd2509
Gloria Odipo <gloriaodipo@gmail.com>
2019-03-25 12:01:22 +0300

    Add mutations

---
+++ app/graphql/mutations/create_link.rb
@@ -0,0 +1,17 @@
+module Mutations
+  class CreateLink < BaseMutation
+    #arguments passed to the resolved method
+    argument :url, String, required: true
+    argument :description, String, required: true
+
+    #return type from the mutation
+    type Types::LinkType
+
+    def resolve(url: nil, description: nil)
+      Link.create!(
+        url: url,
+        description: description
+      )
+    end
+  end
+end
