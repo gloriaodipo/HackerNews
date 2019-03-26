9984cf2e924f35beb2c2be04ed1cddcc484995a3
Gloria Odipo <gloriaodipo@gmail.com>
2019-03-26 12:01:33 +0300

    Error handling

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ApplicationRecord
   belongs_to :user, optional: true #prevents ActiveRecord::RecordInvalid
+  validates :url, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 5 }
   has_many :votes
 end
